01a196f0de0c9a0f1310e088a20bb870f4d1dd94
Shakil000 <racer.shakil111@gmail.com>
2025-07-09 18:21:07 +0600

    setup eslint,dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
@@ -21,3 +22,50 @@ const startServer = async() => {
 
 startServer();
 
+//! server unhandled rejection handled here.................
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled rejection detected...Server shutting down...", err)
+
+    if(server){
+        server.close( () => {
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+});
+// Promise.reject(new Error("I forgot to handle the error"));
+
+// ! server uncaught exception error handled here..................
+process.on("uncaughtException" ,(err) => {
+    console.log("UnCaught exception detected....Server shutting down....", err)
+
+    if(server){
+        server.close( () => {
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+});
+// throw new Error("uncaught exception error");
+
+// ! Signal SIGTERM error handled here..................
+process.on("SIGTERM", () => {
+    console.log("Sigterm signal error.....Server shutting down....")
+    if(server){
+        server.close( () => {
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+})
+
+// ! Signal SIGINT error handled here..................
+process.on("SIGINT", () => {
+    console.log("Sigint signal error.....Server shutting down....")
+    if(server){
+        server.close( () => {
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+})
